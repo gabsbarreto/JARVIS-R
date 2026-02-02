55aad3474baa3993a65132804b537da379a4a46d
gabsbarreto <gabriel.castanhob@usp.br>
2026-02-02 14:17:51 +0000

    including install.r

---
+++ scripts/install.R
@@ -0,0 +1,14 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+
+
+install.packages(c(
+    "tidyverse",
+    "recipes",
+    "textrecipes",
+    "h2o",
+    "future.apply",
+    "tibble",
+    "ggpubr",
+    "PRROC")
+)
